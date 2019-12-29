d33bff464a917e3db6a8342eb885b524999bc06b
Will McGarty <37192274+will986will@users.noreply.github.com>
2019-12-29 11:18:13 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-run = 1
-even = ["0","2","4","6","8"]
-while run == 1:
-  num = str(input("Enter your number: "))
-  numLen = len(num)
-  number = []
-  number.append(num[numLen-1])
-  if number[0] in even:
+run = "1"
+while run == "1":
+  num = int(input("Enter your number: "))
+  if num % 2 == 0:
     print (num,"is even")
   else:
     print (num,"is odd")
-  run = input("Would you like to go again? press 1. If not press Enter")
+  run = str(input("Would you like to go again? press 1. If not press Enter"))
